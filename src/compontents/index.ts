888fb32468050703f7baf104f865c771e9cb81a5
Davichobits <davidruizdeveloper@gmail.com>
2025-09-11 05:47:46 -0500

    Refactor footer components and remove unused MainImage component

--- src/compontents/index.ts
+++ src/compontents/index.ts
@@ -1,9 +1,8 @@
 export  { default as Hero } from './Hero.astro';
 export {default as Features } from './Features.astro';
-export {default as MainImage } from './MainImage.astro';
 export {default as Navbar } from './Navbar.astro';
 export {default as Share } from './Share.astro';
 export {default as Team } from './Team.astro';
-export {default as Footer} from './Footer.astro';
+export {default as Footer} from './footer/Footer.astro';
 export {default as Form} from './Form.astro';
 export {default as Button} from './Button.astro';
